779b2d59fa16a6aa7ed54fb8796496b84be097d3
zengjg <zengjgkeynes@163.com>
2022-07-02 16:19:45 +0800

    fix allow host

--- clickBoardBE/settings.py
+++ clickBoardBE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x*osn)v+s0f7nlhl#2p34%qfxy#c!qeq^oa+jjszs(b6^)iv9c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
